1706af6a13b132f30e15d6e0b3bd5a5f1913f074
Akshey-Gupta <100192799+Akshey-Gupta@users.noreply.github.com>
2024-01-07 14:58:46 +0530

    Add files via upload

---
+++ spotify/script.js
@@ -0,0 +1,6 @@
+// console.log("Welcome to Spotify");
+// let songs={
+//     {songName:'', filePath:'',coverPath}
+// }
+let audioElement=new Audio('songs/Angry_(mood)/1.mp3');
+// audioElement.play();
